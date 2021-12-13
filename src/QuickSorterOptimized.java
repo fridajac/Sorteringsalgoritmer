f3121f1e679d1f64878240be9f0190241e72ad57
fridajac <frida_jacobsson@live.se>
2021-12-13 13:11:12 +0100

    Merge branch into main

--- src/QuickSorterOptimized.java
+++ src/QuickSorterOptimized.java
@@ -1,25 +1,30 @@
 package src;
 
-public class QuickSorterOptimized implements IntSorter {
+public class QuickSorterOptimized implements IntSorter{
 
     public void sort(int[] a) {
         ArrayUtil.shuffle(a); //skapa array och blanda om den
-        int N = a.length - 1; // N är lika med arrayens längd -1
+        int N = a.length-1; // N är lika med arrayens längd -1
         quickSort(a, 0, N);// skicka till metoden quicksort, låg 0, hög N(högsta talet) och arrayen.
     }
 
-    static void swap(int[] arr, int i, int j) {
+    static void swap(int[] arr, int i, int j)
+    {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
     }
 
-    static int partition(int[] arr, int low, int high) {
+    static int partition(int[] arr, int low, int high)
+    {
+
         int pivot = arr[high];
         int i = (low - 1);
 
-        for (int j = low; j <= high; j++) {
-            if (arr[j] < pivot) {
+        for(int j = low; j <= high; j++)
+        {
+            if (arr[j] < pivot)
+            {
                 i++; // Increment index of  smaller element
                 swap(arr, i, j);
             }
@@ -28,27 +33,29 @@ public class QuickSorterOptimized implements IntSorter {
         return (i + 1);
     }
 
-    static void quickSort(int[] arr, int low, int high) {
+    static void quickSort(int[] arr, int low, int high)
+    {
         int M = 500;
-        if (low < high) {
-            if (high - low <= M) {
-                insertionSort(low, high, arr);
+        if (low < high)
+        {
+            if (high-low<=M){
+                insertionSort(low,high,arr);
 
-            }
-            else {
-                int partitioningIndex = partition(arr, low, high);
-                quickSort(arr, low, partitioningIndex - 1);
-                quickSort(arr, partitioningIndex + 1, high);
+            }else {
+
+
+                int partitioningindex = partition(arr, low, high);
+
+                quickSort(arr, low, partitioningindex - 1);
+                quickSort(arr, partitioningindex + 1, high);
             }
         }
     }
 
-    public static void insertionSort(int lo, int hi, int[] a) {
-        for (int i = lo; i < hi + 1; i++) {
-            for (int j = i; j > lo && a[j] < a[j - 1]; j--) {
-                int x = a[j];
-                a[j] = a[j - 1];
-                a[j - 1] = x;
+    public static void insertionSort(int lo, int hi, int[] a){
+        for (int i = lo; i < hi+1; i++) {
+            for (int j = i; j > lo && a[j] < a[j-1]; j--) {
+                int x = a[j]; a[j] = a[j-1]; a[j-1] = x;
             }
         }
     }
